38ba04e9a74680611a2e9934a9c263eaacc06996
alimarcanotovar <aliymarcanotovar91@gmail.com>
2022-09-08 09:15:38 -0500

    Add project files.

---
+++ BestBuyBestPractices/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace BestBuyBestPractices
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
